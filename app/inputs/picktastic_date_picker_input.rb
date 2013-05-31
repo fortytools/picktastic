a758df455dad11638481bbaa61d1ab1fca7dfe99
Tobias Raeder <tobias.raeder@gmail.com>
2013-05-31 12:36:25 +0200

    Support firstDay property and add some default values

--- app/inputs/picktastic_date_picker_input.rb
+++ app/inputs/picktastic_date_picker_input.rb
@@ -1,8 +1,11 @@
 class PicktasticDatePickerInput < Formtastic::Inputs::StringInput
 
-  cattr_accessor :format
+  cattr_accessor :first_day, :format
+
+  @@first_day = 1
+  @@format = I18n.t("date.formats.datepicker")
 
   def input_html_options
-    super.merge(:class => "picktastic_date_picker", :'data-dateformat' => PicktasticDatePickerInput.format)
+    super.merge(:class => "picktastic_date_picker")
   end
 end
