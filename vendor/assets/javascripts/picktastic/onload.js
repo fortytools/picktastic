5532df955e8f0f22d658351e645a3fc33fd8324a
Tobias Raeder <tobias.raeder@gmail.com>
2013-08-21 11:44:40 +0200

    some js refactoring

--- vendor/assets/javascripts/picktastic/onload.js
+++ vendor/assets/javascripts/picktastic/onload.js
@@ -1,24 +1,27 @@
 $(picktastic_init);
 
 picktasticProperties = ['format', 'weekStart', 'autoclose', 'language', 'todayBtn'];
+var addProperty = function(object, input, key) {
+  // haml lowercases all attribute-names so we need to look up the data attributes lowercased
+  var value = input.data(key.toLowerCase());
+  if (value !== undefined) {
+    object[key] = value;
+  }
+};
+
+function initPicktasticForElement(elem) {
+  var input = $(elem);
+  var options = {};
+
+  $.each(picktasticProperties, function(idx, key) {
+    addProperty(options, input, key);
+  });
+  return input.datepicker(options);
+};
 
 function picktastic_init() {
-  var addProperty = function(object, input, key) {
-    // haml lowercases all attribute-names so we need to look up the data attributes lowercased
-    var value = input.data(key.toLowerCase());
-    if (value !== undefined) {
-      object[key] = value;
-    }
-  };
-
   $('input.picktastic_date_picker').each(function(idx, elem) {
-    var input = $(elem);
-    var options = {};
-
-    $.each(picktasticProperties, function(idx, key) {
-      addProperty(options, input, key);
-    });
-    input.datepicker(options);
+    initPicktasticForElement(elem);
   });
-}
+};
 
